fd792629b37d87dd372bb1867e33cae9540877fa
unknown <jeremie229242@gmail.com>
2026-05-28 10:18:40 +0100

    02

--- ftre/app/(tabs)/index.tsx
+++ ftre/app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
-import { View, Text, ScrollView, Image, Dimensions } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, Dimensions, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '@/components/Header'
 import { BANNERS } from '@/assets/assets'
+import { useRouter } from 'expo-router'
 
 const {width} = Dimensions.get("window")
 
 export default function Home() {
+
+  const router = useRouter();
+  const[activeBannerIndex, setActiveBannerIndex] = useState(0)
+
   return (
   <SafeAreaView className='flex-1' edges={['top']}>
 
@@ -15,19 +20,41 @@ export default function Home() {
     <ScrollView className='flex-1 px-4' showsVerticalScrollIndicator={false}>
       {/* slider */}
 
-      <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} className='w-full h-48 rounded-xl' scrollEventThrottle={16}>
+      <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} className='w-full h-48 rounded-xl' scrollEventThrottle={16} 
+      onScroll={(e)=>{
+        const slide = Math.ceil(e.nativeEvent.contentOffset.x / e.nativeEvent.layoutMeasurement.width)
+        if(slide !== activeBannerIndex){
+          setActiveBannerIndex(slide)
+        }
+      }}>
         {BANNERS.map((banner, index)=>(
-          <view key={index} className='relative x-full h-48 bg-gray-200 overflow-hidden' style={{width: width - 32}}>
+          <view key={index} className='relative w-full h-48 bg-gray-200 overflow-hidden' style={{width: width - 32}}>
             <Image source={{uri: banner.image}} className='w-full h-full' resizeMode='cover'/>
-            <view>
-
-              
+            <view className='absolute bottom-4 left-4 z-10'>
+            <Text className='text-white text-2xl font-bold'>{banner.title}</Text>
+            <TouchableOpacity></TouchableOpacity>
+            <Text className='text-white text-sm font-medium'>{banner.subtitle}</Text>
+            <TouchableOpacity className='mt-2 bg-white px-4 py-2 rounded-full self-start'>
+              <Text className='text-primary font-bold text-xs'>Acheter Maintenent</Text>
+              </TouchableOpacity>
             </view>
+
+            <View className='absolute inset-0 bg-black/40'/>
+            
+             
 
           </view>
         ))}
 
       </ScrollView>
+
+      {/* Pagination */}
+      <view className='flex-row justify-center mt-3 gap-2'>
+
+        {BANNERS.map((_, index)=>(
+          <view key={index} className={`h-2 rounded-full ${index === activeBannerIndex ? 'w-6 bg-primary': 'w-2 bg-gray-300' }`}/>
+        ))}
+      </view>
 
     </ScrollView>
 
